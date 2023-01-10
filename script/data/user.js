f2298c48871168c2e7fde4796f0932e253ce3d38
Na Lin <lna483018@gmail.com>
2023-01-10 16:37:17 -0500

    create(script): add user dummy data

---
+++ script/data/user.js
@@ -0,0 +1,41 @@
+module.exports = [
+  {
+    name: "John",
+    email: "john@example.com",
+    password: "123456",
+    passwordConfirm: "123456",
+  },
+  {
+    name: "Anne",
+    email: "anne@example.com",
+    password: "123456",
+    passwordConfirm: "123456",
+  },
+  {
+    name: "Tessa",
+    email: "tessa@example.com",
+    password: "123456",
+    passwordConfirm: "123456",
+  },
+  {
+    name: "Joy",
+    email: "joy@example.com",
+    password: "123456",
+    passwordConfirm: "123456",
+  },
+  {
+    name: "Cindy",
+    email: "cindy@example.com",
+    password: "123456",
+    passwordConfirm: "123456",
+    role: "admin",
+  },
+  {
+    name: "poppy",
+    email: "poppy@example.com",
+    password: "123456",
+    passwordConfirm: "123456",
+    role: "user",
+    active: "false",
+  },
+];
