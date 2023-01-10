2e9334248a85dc030283a85bb0d202d5424b3d4c
madeleine-lloyd-davies <mc.lloyd.davies@gmail.com>
2023-01-10 17:00:22 -0500

    feat: define Product model

---
+++ server/db/models/productName.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+const db = require('./db');
+
+const Product = db.define('product', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      'https://cdn.britannica.com/24/189624-050-F3C5BAA9/Mona-Lisa-oil-wood-panel-Leonardo-da.jpg?w=300&h=169&c=crop',
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  description: {
+    type: Sequelize.TEXT,
+  },
+  price: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  artistName: {
+    type: Sequelize.STRING,
+  },
+  stockCount: {
+    type: Sequelize.INTEGER,
+  },
+});
+module.exports = Product;
