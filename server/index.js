7128b5aac618e18bbf048db8b1e2ccfab1ac6ed2
Na Lin <lna483018@gmail.com>
2023-01-10 22:02:46 -0500

    update: update env path

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './dev.env' });
+dotenv.config({ path: './.env' });
 const colors = require('colors');
 const { db } = require('./db');
 const PORT = process.env.PORT || 8080;
