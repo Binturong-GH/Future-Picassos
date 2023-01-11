3eb756d98f8fbfbc28496881dd18d778d1508533
madeleine-lloyd-davies <mc.lloyd.davies@gmail.com>
2023-01-10 23:07:36 -0500

    refactor: import Cart model on db/index

--- server/db/index.js
+++ server/db/index.js
@@ -1,15 +1,16 @@
 //this is the access point for all things database related!
 
-const db = require("./db");
+const db = require('./db');
 
-const templateUser = require("./models/User");
-const User = require("./models/userModel");
-const Product = require("./models/productModel");
-const Order = require("./models/orderModel");
+const templateUser = require('./models/User');
+const User = require('./models/userModel');
+const Product = require('./models/productModel');
+const Order = require('./models/orderModel');
+const Cart = require('.models/cartModel');
 
 //associations could go here!
-Product.belongsToMany(User, { through: "Cart" });
-User.belongsToMany(Product, { through: "Cart" });
+Product.belongsToMany(User, { through: 'Cart' });
+User.belongsToMany(Product, { through: 'Cart' });
 
 User.hasMany(Order);
 Order.belongsTo(User);
