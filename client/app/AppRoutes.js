b37a0ccf975a45e009b5b478b190173293f9455c
madeleine-lloyd-davies <mc.lloyd.davies@gmail.com>
2023-01-11 21:59:51 -0500

    FEAT: add cart page to AppRoutes.js

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Cart from './pages/Cart';
 
 const AppRoutes = () => {
-  return <div>Welcome to Grace shopper</div>;
+  return (
+    <div>
+      <main>
+        <Route path='/cart' element={<Cart />} />
+      </main>
+    </div>
+  );
 };
 
 export default AppRoutes;
