578da474665e790a4f2e5b870d3aa0e7b6f632a0
rachelcorawood <rachelcorawood@gmail.com>
2023-01-12 15:56:16 -0500

    Nav Bar, All Products & Individual Products Components, Slices, and Backend

--- client/app/App.js
+++ client/app/App.js
@@ -1,15 +1,12 @@
-import React from 'react';
-
-//import Navbar from './components/Navbar';
-import AppRoutes from './AppRoutes';
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import AppRoutes from "./AppRoutes";
 
 const App = () => {
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       <AppRoutes />
-
     </div>
   );
 };
--- client/app/components/Navbar.js
+++ client/app/components/Navbar.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, IconButton, Button } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
-import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { Stack } from "@mui/system";
 
 const Navbar = () => {
@@ -22,13 +23,22 @@ const Navbar = () => {
             start
             aria-label="label"
           >
-            <PermIdentityIcon/>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <PermIdentityIcon />
+            <Typography variant="h8" component="div" sx={{ flexGrow: 1 }}>
               User Profile
             </Typography>
           </IconButton>
-          <Button color="primary">Cart</Button>
-          <Button color="primary">Contact</Button>
+          <IconButton
+            size="small"
+            color="primary edge="
+            start
+            aria-label="label"
+          >
+            <ShoppingCartIcon />
+            <Typography variant="h8" component="div" sx={{ flexGrow: 1 }}>
+              Cart
+            </Typography>
+          </IconButton>
         </Stack>
       </Toolbar>
     </AppBar>
