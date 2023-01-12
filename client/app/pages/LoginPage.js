35fa6d01d961df9d3a16b77696c4f98288cdbd0a
Na Lin <lna483018@gmail.com>
2023-01-12 17:01:45 -0500

    feat: create login form

---
+++ client/app/pages/LoginPage.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Box, Button, TextField, Typography } from '@mui/material';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
+const valiate = yup.object({
+  email: yup
+    .string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: yup
+    .string('Enter your password')
+    .min(8, 'Password should be of mininum 8 characters length')
+    .required('Password is required'),
+});
+
+export default function LoginPage() {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    },
+    validationSchema: valiate,
+    onSubmit: (values) => {
+      dispatch(signup(values));
+    },
+  });
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <Box display={'flex'} flexDirection={'column'}>
+          <Typography>Log In</Typography>
+          <TextField
+            fullWidth
+            id='email'
+            name='email'
+            label='Email'
+            type='email'
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+          />
+          <TextField
+            fullWidth
+            id='password'
+            name='password'
+            label='Password'
+            type='password'
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+          />
+          <Button color='primary' variant='contained' fullWidth type='submit'>
+            Sign up
+          </Button>
+        </Box>
+      </form>
+    </div>
+  );
+}
