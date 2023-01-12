6fcbb5a52ac2c63ba829e53f4fcea188663055ff
madeleine-lloyd-davies <mc.lloyd.davies@gmail.com>
2023-01-12 16:28:39 -0500

    feat: build out remaining CRUD routes in cart api

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -11,5 +11,31 @@ exports.getUserCart = catchAsync(async (req, res, next) => {
   const cartItems = await Cart.findAll({
     where: { userId: req.userId },
   });
-  res.send(cartItems);
+  res.json(cartItems);
+});
+
+//POST api/cart
+exports.addToCart = catchAsync(async (req, res, next) => {
+  res.json(await Cart.create(req.body));
+});
+
+//PUT api/cart
+exports.editQuantity = catchAsync(async (req, res, next) => {
+  const [quant, item] = await Cart.update(req.body, {
+    where: {
+      userId: req.userId,
+    },
+  });
+  res.json(item);
+});
+
+//DELETE api/cart
+exports.deleteItem = catchAsync(async (req, res, next) => {
+  const deletedItem = await Cart.destroy({
+    where: {
+      userId: req.userId,
+      productId: req.body.productId,
+    },
+  });
+  res.json(deletedItem);
 });
