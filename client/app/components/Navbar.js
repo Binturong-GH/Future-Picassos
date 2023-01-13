f2793ccc79252d4a028785898892f41d0ce6b1be
Na Lin <lna483018@gmail.com>
2023-01-12 19:50:30 -0500

    format: change everything to single quote

--- client/app/components/Navbar.js
+++ client/app/components/Navbar.js
@@ -1,41 +1,41 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, IconButton, Button } from "@mui/material";
-import HomeIcon from "@mui/icons-material/Home";
-import PermIdentityIcon from "@mui/icons-material/PermIdentity";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import { Stack } from "@mui/system";
+import React from 'react';
+import { AppBar, Toolbar, Typography, IconButton, Button } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Stack } from '@mui/system';
 
 const Navbar = () => {
   return (
-    <AppBar color="secondary" position="stick">
+    <AppBar color='secondary' position='stick'>
       <Toolbar>
-        <IconButton size="small" color="primary edge=" start aria-label="label">
+        <IconButton size='small' color='primary edge=' start aria-label='label'>
           <HomeIcon />
         </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
           Grace Shopper
         </Typography>
 
-        <Stack direction="row" spacing={2}>
+        <Stack direction='row' spacing={2}>
           <IconButton
-            size="small"
-            color="primary edge="
+            size='small'
+            color='primary edge='
             start
-            aria-label="label"
+            aria-label='label'
           >
             <PermIdentityIcon />
-            <Typography variant="h8" component="div" sx={{ flexGrow: 1 }}>
+            <Typography variant='h8' component='div' sx={{ flexGrow: 1 }}>
               User Profile
             </Typography>
           </IconButton>
           <IconButton
-            size="small"
-            color="primary edge="
+            size='small'
+            color='primary edge='
             start
-            aria-label="label"
+            aria-label='label'
           >
             <ShoppingCartIcon />
-            <Typography variant="h8" component="div" sx={{ flexGrow: 1 }}>
+            <Typography variant='h8' component='div' sx={{ flexGrow: 1 }}>
               Cart
             </Typography>
           </IconButton>
