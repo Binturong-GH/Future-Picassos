4cb1dab4c84c36ef55fa47023f83f4f9ab82022a
rachelcorawood <rachelcorawood@gmail.com>
2023-01-13 15:28:52 -0500

    small edits for the main

--- client/app/components/SingleProduct.js
+++ client/app/components/SingleProduct.js
@@ -3,8 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchOneProductAsync } from "../store/slices/singleProductSlice";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
-import { IconButton, Typography} from "@mui/material";
-
+import { IconButton, Typography } from "@mui/material";
 
 function SingleProduct() {
   const dispatch = useDispatch();
@@ -18,22 +17,26 @@ function SingleProduct() {
 
   return (
     <div>
-      <img alt='product image' src={product.imageUrl} />
+      <img alt="product image" src={product.imageUrl} />
       <h3>{product.title}</h3>
       <h3>Artist: {product.artistName}</h3>
 
       <h4>Description: {product.description} </h4>
-      <p>
-         ${product.price}
-      </p>
-      <IconButton  onClick={() => {
+      <p>${product.price}</p>
+      <IconButton
+        onClick={() => {
           dispatch(addToCart(product));
-        } size="small" color="primary edge=" start aria-label="label">
-          <AddShoppingCartIcon />
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Add to cart
-          </Typography>
-          </IconButton>
+        }}
+        size="small"
+        color="primary edge="
+        start
+        aria-label="label"
+      >
+        <AddShoppingCartIcon />
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Add to cart
+        </Typography>
+      </IconButton>
       <div></div>
     </div>
   );
