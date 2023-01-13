6ad86b0ccee53ab926bdc1f55667ef5ca7050303
Na Lin <lna483018@gmail.com>
2023-01-12 20:06:36 -0500

    feat: add dropdown menu in nav bar

--- client/app/components/Navbar.js
+++ client/app/components/Navbar.js
@@ -1,47 +1,83 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton, Button } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Button,
+  Menu,
+  MenuList,
+} from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Stack } from '@mui/system';
 
 const Navbar = () => {
-  return (
-    <AppBar color='secondary' position='stick'>
-      <Toolbar>
-        <IconButton size='small' color='primary edge=' start aria-label='label'>
-          <HomeIcon />
-        </IconButton>
-        <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-          Grace Shopper
-        </Typography>
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-        <Stack direction='row' spacing={2}>
+  return (
+    <>
+      <AppBar color='secondary' position='stick'>
+        <Toolbar>
           <IconButton
             size='small'
             color='primary edge='
             start
             aria-label='label'
           >
-            <PermIdentityIcon />
-            <Typography variant='h8' component='div' sx={{ flexGrow: 1 }}>
-              User Profile
-            </Typography>
+            <HomeIcon />
           </IconButton>
-          <IconButton
-            size='small'
-            color='primary edge='
-            start
-            aria-label='label'
-          >
-            <ShoppingCartIcon />
-            <Typography variant='h8' component='div' sx={{ flexGrow: 1 }}>
-              Cart
-            </Typography>
-          </IconButton>
-        </Stack>
-      </Toolbar>
-    </AppBar>
+          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+            Grace Shopper
+          </Typography>
+
+          <Stack direction='row' spacing={2}>
+            <Button
+              id='basic-button'
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup='true'
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+            >
+              <PermIdentityIcon />
+            </Button>
+
+            <IconButton
+              size='small'
+              color='primary edge='
+              start
+              aria-label='label'
+            >
+              <ShoppingCartIcon />
+              <Typography variant='h8' component='div' sx={{ flexGrow: 1 }}>
+                Cart
+              </Typography>
+            </IconButton>
+          </Stack>
+        </Toolbar>
+      </AppBar>
+
+      <Menu
+        id='basic-menu'
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuList onClick={handleClose}>Profile</MenuList>
+        <MenuList onClick={handleClose}>Logout</MenuList>
+      </Menu>
+    </>
   );
 };
 
