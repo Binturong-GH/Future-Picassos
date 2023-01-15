801acd658d401314dde00c38a1bdc079a69d8800
Na Lin <lna483018@gmail.com>
2023-01-15 12:05:18 -0500

    fix: fix promise states

--- client/app/store/slices/usersSlice.js
+++ client/app/store/slices/usersSlice.js
@@ -32,11 +32,11 @@ const usersSlice = createSlice({
     builder.addCase(getAllUsers.pending, (state, action) => {
       state.isLoading = true;
     });
-    builder.addCase(getAllUsers.pending, (state, action) => {
+    builder.addCase(getAllUsers.fulfilled, (state, action) => {
       state.isLoading = false;
       state.users = action.payload.users;
     });
-    builder.addCase(getAllUsers.pending, (state, action) => {
+    builder.addCase(getAllUsers.rejected, (state, action) => {
       state.isLoading = false;
       state.error = action.error.message;
     });
