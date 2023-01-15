6ea331cd81eb14232611c99e4e716471a5b3065e
Na Lin <lna483018@gmail.com>
2023-01-15 11:54:11 -0500

    fix: fix request loading state value

--- client/app/store/slices/usersSlice.js
+++ client/app/store/slices/usersSlice.js
@@ -33,10 +33,11 @@ const usersSlice = createSlice({
       state.isLoading = true;
     });
     builder.addCase(getAllUsers.pending, (state, action) => {
+      state.isLoading = false;
       state.users = action.payload.users;
     });
     builder.addCase(getAllUsers.pending, (state, action) => {
-      state.isLoading = true;
+      state.isLoading = false;
       state.error = action.error.message;
     });
   },
