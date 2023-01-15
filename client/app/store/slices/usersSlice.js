452e76a42c9f1a1d5b9964b93333921fb3f5e03d
Na Lin <lna483018@gmail.com>
2023-01-15 13:47:14 -0500

    feat: add redux to delete user

--- client/app/store/slices/usersSlice.js
+++ client/app/store/slices/usersSlice.js
@@ -20,6 +20,24 @@ export const getAllUsers = createAsyncThunk("admin/getAllUsers", async () => {
   }
 });
 
+// @desc: Admin delete user
+export const deleteUser = createAsyncThunk("admin/deleteUser", async (id) => {
+  try {
+    const token = JSON.parse(localStorage.getItem("jwt"));
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const res = await axios.delete(`/api/users/${id}`, config);
+    return id;
+  } catch (error) {
+    const errMsg = error.response.data;
+    throw new Error(errMsg);
+  }
+});
+
 const usersSlice = createSlice({
   name: "users",
   initialState: {
@@ -45,6 +63,22 @@ const usersSlice = createSlice({
     // add case for user logout
     builder.addCase(logout, (state, action) => {
       state.users = [];
+    });
+
+    //delete user
+    builder.addCase(deleteUser.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(deleteUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      const restedUser = state.users.filter(
+        (user) => user.id !== action.payload.id
+      );
+      state.users = restedUser;
+    });
+    builder.addCase(deleteUser.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message;
     });
   },
 });
