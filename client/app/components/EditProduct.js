dd9446e8737b8146a44bf7c8a272a628710ba229
Na Lin <lna483018@gmail.com>
2023-01-16 13:14:36 -0500

    add: add edit product form to dialog

--- client/app/components/EditProduct.js
+++ client/app/components/EditProduct.js
@@ -1,30 +1,23 @@
 import React, { useEffect } from "react";
 import {
-  Modal,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   Box,
-  Typography,
   Alert,
   Backdrop,
   CircularProgress,
+  TextField,
+  Button,
 } from "@mui/material";
 
-import { useFormik } from "formik";
+import { Field, useFormik } from "formik";
 import * as yup from "yup";
 
 import { useDispatch, useSelector } from "react-redux";
 import { fetchOneProductAsync } from "../store";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
 
 const validate = yup.object({
   title: yup
@@ -84,21 +77,129 @@ export default function EditProduct({ handleClose, open, id }) {
 
   return (
     <div>
-      <Modal
+      <Dialog
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
+        <DialogTitle>Update product</DialogTitle>
+        <form onSubmit={formik.handleSubmit}>
+          <DialogContent>
+            <Box display={"flex"} flexDirection={"column"}>
+              <TextField
+                fullWidth
+                id="title"
+                name="title"
+                label="title"
+                type="text"
+                value={formik.values.title}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.title && Boolean(formik.errors.title)}
+                helperText={formik.touched.title && formik.errors.title}
+              />
+              <TextField
+                fullWidth
+                id="imageUrl"
+                name="imageUrl"
+                label="Image URL"
+                type="text"
+                value={formik.values.imageUrl}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.imageUrl && Boolean(formik.errors.imageUrl)
+                }
+                helperText={formik.touched.imageUrl && formik.errors.imageUrl}
+              />
+              <TextField
+                fullWidth
+                multiline
+                rows={4}
+                id="description"
+                placeholder="description"
+                name="description"
+                label="description"
+                value={formik.values.description}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.description &&
+                  Boolean(formik.errors.description)
+                }
+                helperText={
+                  formik.touched.description && formik.errors.description
+                }
+              />
+              <TextField
+                fullWidth
+                id="price"
+                name="price"
+                label="Price"
+                type="number"
+                inputProps={{
+                  step: 0.01,
+                }}
+                value={formik.values.price}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.price && Boolean(formik.errors.price)}
+                helperText={formik.touched.price && formik.errors.price}
+              />
+              <TextField
+                fullWidth
+                id="artistName"
+                name="artistName"
+                label="Artist Name"
+                type="text"
+                value={formik.values.artistName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.artistName && Boolean(formik.errors.artistName)
+                }
+                helperText={
+                  formik.touched.artistName && formik.errors.artistName
+                }
+              />
+              <TextField
+                fullWidth
+                id="age"
+                name="age"
+                label="age"
+                type="number"
+                value={formik.values.age}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.age && Boolean(formik.errors.age)}
+                helperText={formik.touched.age && formik.errors.age}
+              />
+              <TextField
+                fullWidth
+                id="countInStock"
+                name="countInStock"
+                label="Count In Stock"
+                type="number"
+                value={formik.values.countInStock}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.countInStock &&
+                  Boolean(formik.errors.countInStock)
+                }
+                helperText={
+                  formik.touched.countInStock && formik.errors.countInStock
+                }
+              />
+            </Box>
+          </DialogContent>
+          <DialogActions>
+            <Button type="submit">Update</Button>
+            <Button onClick={handleClose}>Cancel</Button>
+          </DialogActions>
+        </form>
+      </Dialog>
     </div>
   );
 }
