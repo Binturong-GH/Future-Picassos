1074ec6901d4ff980f829aa0347adf47b206549a
Na Lin <lna483018@gmail.com>
2023-01-16 12:10:02 -0500

    update: change product to id to get more product detail

--- client/app/pages/ProductsListPage.js
+++ client/app/pages/ProductsListPage.js
@@ -143,7 +143,7 @@ export default function ProductsListPage() {
           <EditProduct
             handleClose={handleEditProductPromptClose}
             open={openEditProductPrompt}
-            product={productWillBeEdit}
+            id={productWillBeEdit}
           />
         )}
       </>
