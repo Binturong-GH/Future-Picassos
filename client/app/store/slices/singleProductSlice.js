610213c401fe58d22d0b38dbbcf74303275897aa
Na Lin <lna483018@gmail.com>
2023-01-16 12:19:20 -0500

    del: delete thunk that covered at productsSlice

--- client/app/store/slices/singleProductSlice.js
+++ client/app/store/slices/singleProductSlice.js
@@ -17,50 +17,6 @@ export const fetchOneProductAsync = createAsyncThunk(
   }
 );
 
-//TODO: Need to make back end for this
-
-// export const addNewProductAsync = createAsyncThunk(
-//   "product/addProduct",
-//   async ({ title, price, imageUrl}) => {
-//     try {
-//       const { data } = await axios.post("/api/campuses", { title , price, imageUrl });
-//       return data;
-//     } catch (error) {
-//       console.log(error);
-//       next(error);
-//     }
-//   }
-// );
-
-// export const updateProductAsync = createAsyncThunk(
-//   "product/update",
-//   async (product) => {
-//     try {
-//       console.log(product);
-//       const response = await axios.put(`/api/product/${product.id}`, product);
-//       console.log(response.data);
-//       return response.data;
-//     } catch (error) {
-//       console.log(error);
-//       next(error);
-//     }
-//   }
-// );
-
-// export const deleteProductAsync = createAsyncThunk(
-//   "product/delete",
-//   async (productId) => {
-//     try {
-//       console.log(productId);
-//       const { data } = await axios.delete(`/api/product/${productId}`);
-//       return data;
-//     } catch (error) {
-//       console.log(error);
-//       next(error);
-//     }
-//   }
-// );
-
 const singleProductSlice = createSlice({
   name: "product",
   initialState: {
@@ -83,17 +39,6 @@ const singleProductSlice = createSlice({
 
       state.error = action.error.message;
     });
-    //  .addCase(addNewProductAsync.fulfilled, (state, action) => {
-    //     state.product.push(action.payload);
-    //   })
-    //   .addCase(deleteProductAsync.fulfilled, (state, action) => {
-    //     state.product = state.product.filter((product) => {
-    //       return product.id !== action.meta.arg;
-    //     });
-    //   })
-    //   .addCase(updateProductAsync.fulfilled, (state, action) => {
-    //     state.product = action.payload;
-    //   });
   },
 });
 
