f8709cf7179914883d51135866f7d4907f441800
rachelcorawood <rachelcorawood@gmail.com>
2023-01-17 17:48:10 -0500

    adjusted our navBar and Home Page to Future Picassos

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -61,9 +61,9 @@ const AppRoutes = () => {
 
   return (
     <main>
-      <h1 className="title">Welcome to Grace shopper</h1>
+      <h1 className="title"></h1>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<h1>Welcome to Future Picassos</h1>} />
         <Route path="/products" element={<ProductsList />} />
         <Route path="/products/:productId" element={<SingleProduct />} />
         <Route path="/cart" element={<Cart />} />
