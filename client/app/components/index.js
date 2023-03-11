3cfe40c406aa42f28e1096848e02dfb1c6b9c176
Na Lin <lna483018@gmail.com>
2023-03-11 00:59:11 -0500

    add error component

--- client/app/components/index.js
+++ client/app/components/index.js
@@ -1,4 +1,5 @@
 import ProductItem from "./ProductItem";
 import LoadingSpinner from "./LoadingSpinner";
+import Error from "./Error";
 
-export { ProductItem, LoadingSpinner };
+export { ProductItem, LoadingSpinner, Error };
