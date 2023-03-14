fc8bf996c42b80b02b9d5da9c48adf48383668a6
Na Lin <lna483018@gmail.com>
2023-03-14 14:52:54 -0400

    add art frame to each art in all product page

--- client/app/components/ProductItem.js
+++ client/app/components/ProductItem.js
@@ -11,6 +11,24 @@ import { Link as RouterLink } from "react-router-dom";
 import { Box, Stack, Typography, Link, Button } from "@mui/material";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
+const styles = {
+  imageContainer: {
+    height: 233,
+    width: 300,
+    backgroundImage: "url(./readme/frame-width.png)",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "contain",
+    backgroundPosition: "center",
+    position: "relative",
+  },
+  img: {
+    position: "absolute",
+    height: "170px",
+    width: "80%",
+    translate: "30px 33px",
+  },
+};
+
 const ProductItem = ({ product }) => {
   const dispatch = useDispatch();
 
@@ -19,7 +37,7 @@ const ProductItem = ({ product }) => {
   };
 
   return (
-    <Box
+    <Stack
       sx={{
         width: "300px",
         border: 1,
@@ -29,53 +47,44 @@ const ProductItem = ({ product }) => {
         pb: 2,
       }}
     >
-      <Stack>
+      <Link
+        component={RouterLink}
+        to={`/products/${product.id}`}
+        underline="hover"
+      >
+        <Box style={styles.imageContainer}>
+          <img alt={product.title} src={product.imageUrl} style={styles.img} />
+        </Box>
+      </Link>
+
+      {/* Product name & add to cart */}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          px: 1,
+          mt: 2,
+        }}
+      >
         <Link
           component={RouterLink}
           to={`/products/${product.id}`}
           underline="hover"
         >
-          <Box
-            component="img"
-            alt={product.title}
-            src={product.imageUrl}
-            sx={{
-              height: 233,
-              width: 350,
-              maxHeight: { xs: 200, md: 233 },
-              maxWidth: { xs: 180, md: 250, lg: 300 },
-            }}
-          />
+          <Stack>
+            <Typography>{product.title}</Typography>
+            <Typography>${product.price}</Typography>
+          </Stack>
         </Link>
 
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            px: 1,
-            mt: 2,
-          }}
-        >
-          <Link
-            component={RouterLink}
-            to={`/products/${product.id}`}
-            underline="hover"
-          >
-            <Stack>
-              <Typography>{product.title}</Typography>
-              <Typography>${product.price}</Typography>
-            </Stack>
-          </Link>
-
-          <Button onClick={handleAdd}>
-            <Typography>
-              <AddShoppingCartIcon />
-            </Typography>
-          </Button>
-        </Box>
-      </Stack>
-    </Box>
+        <Button onClick={handleAdd}>
+          <Typography>
+            <AddShoppingCartIcon />
+          </Typography>
+        </Button>
+      </Box>
+    </Stack>
   );
 };
 
