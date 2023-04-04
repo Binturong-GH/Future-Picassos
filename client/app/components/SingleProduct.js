90318491a2cf6c583fd0a80ffa592aad3a51fb2b
Na Lin <lna483018@gmail.com>
2023-04-04 19:52:24 -0400

    update the website design

--- client/app/components/SingleProduct.js
+++ client/app/components/SingleProduct.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams, Link } from "react-router-dom";
 import { fetchOneProductAsync } from "../store/slices/singleProductSlice";
@@ -10,14 +10,38 @@ import {
   setLocalCart,
   getLocalCart,
 } from "../store/slices/cartSlice";
+
+// MUI
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import ImageListItem from "@mui/material/ImageListItem";
+import Typography from "@mui/material/Typography";
+
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
-import {
-  IconButton,
-  Typography,
-  Alert,
-  Backdrop,
-  CircularProgress,
-} from "@mui/material";
+import { IconButton, Alert, Backdrop, CircularProgress } from "@mui/material";
+
+const styles = {
+  container: {
+    pt: 1,
+    pb: 4,
+  },
+  flexContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  imageContainer: { width: "50%" },
+  inforContainer: {
+    width: "50%",
+    pl: 8,
+    display: "flex",
+    flexDirection: "column",
+  },
+  spaceY: {
+    mb: 2,
+  },
+};
 
 function SingleProduct() {
   const dispatch = useDispatch();
@@ -75,28 +99,71 @@ function SingleProduct() {
   }
 
   return (
-    <div>
-      <img alt="product image" src={product.imageUrl} />
-      <h3>{product.title}</h3>
-      <h3>Artist: {product.artistName}</h3>
+    <Container maxWidth="lg" sx={styles.container}>
+      <Button variant="contained" sx={{ my: 4 }}>
+        back to Art
+      </Button>
+      <Box sx={styles.flexContainer}>
+        {/* Image */}
+        <Box sx={styles.imageContainer}>
+          <ImageListItem key={product.id}>
+            <img
+              src={product.imageUrl}
+              srcSet={product.imageUrl}
+              alt={product.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        </Box>
+        {/* Product detail */}
+        <Box sx={styles.inforContainer}>
+          <Typography variant="h4" sx={styles.spaceY}>
+            Title: {product.title}
+          </Typography>
+          <Typography variant="h5" sx={styles.spaceY}>
+            Artist: {product.artistName}
+          </Typography>
+          <Typography variant="h5" sx={styles.spaceY}>
+            ${product.price}
+          </Typography>
+          <Typography variant="h5" sx={styles.spaceY}>
+            Description:
+          </Typography>
+          <Typography variant="body1" sx={styles.spaceY}>
+            {product.description}{" "}
+          </Typography>
 
-      <h4>Description: {product.description} </h4>
-      <p>${product.price}</p>
-      <IconButton
-        onClick={handleAdd}
-        size="small"
-        color="primary"
-        edge="start"
-        aria-label="label"
-      >
-        <AddShoppingCartIcon />
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Add to cart
-        </Typography>
-      </IconButton>
-      <div></div>
-    </div>
+          {/* CTA: Add to Cart */}
+          <Button variant="contained" onClick={handleAdd}>
+            Add to cart
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
 }
 
 export default SingleProduct;
+/*
+<div>
+<img alt="product image" src={product.imageUrl} />
+<h3>{product.title}</h3>
+<h3>Artist: {product.artistName}</h3>
+
+<h4>Description: {product.description} </h4>
+<p>${product.price}</p>
+<IconButton
+  onClick={handleAdd}
+  size="small"
+  color="primary"
+  edge="start"
+  aria-label="label"
+>
+  <AddShoppingCartIcon />
+  <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+    Add to cart
+  </Typography>
+</IconButton>
+<div></div>
+</div>
+*/
